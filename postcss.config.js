a39a1c266f532eb28f69b345da364dacb4053f90
Kaspar Arme <kaspar.arme@reach-u.com>
2020-02-12 09:32:56 +0200

    Update stylesheet generation

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,17 @@
-const tailwindcss = require("tailwindcss");
-const purgecss = require("@fullhuman/postcss-purgecss");
+const tailwindcss = require('tailwindcss');
+const purgecss = require('@fullhuman/postcss-purgecss');
+const cssnano = require('cssnano');
+
 module.exports = {
   plugins: [
-    tailwindcss("./tailwind.js"),
-    require("autoprefixer"),
-    ...(process.env.NODE_ENV === "production"
-      ? [
-          purgecss({
-            content: ["./src/**/*.html", "./src/**/*.tsx"],
-            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-          })
-        ]
-      : [])
+    tailwindcss('./tailwind.js'),
+    process.env.NODE_ENV === 'production' ? require('autoprefixer') : null,
+    process.env.NODE_ENV === 'production' ? cssnano({preset: 'default'}) : null,
+    process.env.NODE_ENV === 'production'
+      ? purgecss({
+          content: ['./public/index.html', './src/**/*.tsx'],
+          defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+        })
+      : null
   ]
 };
