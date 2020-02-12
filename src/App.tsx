9f38fb01df109e11dde3d9aa31b752d68367ef9f
Kaspar Arme <kaspar.arme@reach-u.com>
2020-02-12 13:04:14 +0200

    Add basic login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Login from './Login';
 
 const App: React.FC = () => {
-  return <div>Treaty app</div>;
+  return (
+    <div className="w-full h-full text-gray-900">
+      <h1>Treaty logo</h1>
+      <Login />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/Login/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Login: React.FC = () => {
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  return (
+    <form className="flex flex-col items-stretch justify-center h-full px-3">
+      <h1 className="text-2xl text-center mb-3">Log in to Treaty</h1>
+      <div>
+        <label className="block" htmlFor="username">
+          Username
+        </label>
+        <input
+          id="username"
+          className="border border-gray-700 rounded-sm px-1 w-full"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+      </div>
+      <div className="mt-2">
+        <label className="block" htmlFor="password">
+          Password
+        </label>
+        <input
+          id="password"
+          className="border border-gray-700 rounded-sm px-1 w-full"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          type="password"
+        />
+      </div>
+      <button className="bg-gray-300 mt-3 p-3 text-xl rounded-sm">Login</button>
+    </form>
+  );
+};
+
+export default Login;
