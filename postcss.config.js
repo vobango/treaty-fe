05efe1a7c6b595377080a260a328dc59121ad353
Kaspar Arme <kaspar.arme@reach-u.com>
2020-02-12 09:48:33 +0200

    Fix style generation

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,13 @@
 const tailwindcss = require('tailwindcss');
 const purgecss = require('@fullhuman/postcss-purgecss');
-const cssnano = require('cssnano');
 
 module.exports = {
   plugins: [
     tailwindcss('./tailwind.js'),
-    process.env.NODE_ENV === 'production' ? require('autoprefixer') : null,
-    process.env.NODE_ENV === 'production' ? cssnano({preset: 'default'}) : null,
-    process.env.NODE_ENV === 'production'
-      ? purgecss({
-          content: ['./public/index.html', './src/**/*.tsx'],
-          defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-        })
-      : null
+    require('autoprefixer'),
+    purgecss({
+      content: ['./public/index.html', './src/**/*.tsx'],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+    })
   ]
 };
