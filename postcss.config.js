885b1bcf738104467fd583872be12d05c16bd9d2
Kaspar Arme <kaspar.arme@reach-u.com>
2020-02-12 10:16:31 +0200

    Use PurgeCSS only in production

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,17 @@
 const tailwindcss = require('tailwindcss');
 const purgecss = require('@fullhuman/postcss-purgecss');
 
-module.exports = {
-  plugins: [
-    tailwindcss('./tailwind.js'),
-    require('autoprefixer'),
-    purgecss({
-      content: ['./public/index.html', './src/**/*.tsx'],
-      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-    })
-  ]
+module.exports = context => {
+  return {
+    plugins: [
+      tailwindcss('./tailwind.js'),
+      require('autoprefixer'),
+      context.env === 'production'
+        ? purgecss({
+            content: ['./public/index.html', './src/**/*.tsx'],
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+          })
+        : null
+    ]
+  };
 };
