9cc2344770cc5b8d23eccbd50c139c6985ca1365
Kaspar Arme <kaspar.arme@reach-u.com>
2020-04-04 16:56:06 +0300

    Add collapsed menu for smaller devices

--- src/LandingPage/index.js
+++ src/LandingPage/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from '../assets/images/logo.png';
 import landing1 from '../assets/images/landing_1.jpg';
 import {useLocale} from '../context/locale';
+import {useMediaQuery} from 'react-responsive';
+import {screens} from '../utils/constants';
 
 export default () => {
   const {translate} = useLocale();
@@ -13,30 +15,48 @@ export default () => {
     {text: 'workerOffers', to: '/'},
     {text: 'contact', to: '/'}
   ];
+  const isDesktop = useMediaQuery(
+    {
+      minWidth: screens.DESKTOP_MIN_WIDTH
+    },
+    undefined,
+    matches => {
+      setMenuVisibility(matches);
+    }
+  );
+  const [menuVisible, setMenuVisibility] = React.useState(isDesktop);
+
   return (
     <div className="w-full">
       <img className="absolute content-image" src={landing1} alt="workers" />
       <div className="w-full h-48 bg-black opacity-75 absolute" />
       <div className="w-full py-6 px-16 relative z-10 flex items-baseline justify-between">
         <a href="/">
-          <img className="w-auto h-32" src={logo} alt={translate('logo')} />
+          <img className="w-auto max-h-32" src={logo} alt={translate('logo')} />
         </a>
-        <nav>
-          <ul className="flex">
-            {menuItems.map(({text, to}) => {
-              return (
-                <li className="mx-6 text-white font-bold text-xl" key={text}>
-                  <a
-                    href={to}
-                    className="border-0 border-b-2 border-transparent hover:border-white"
-                  >
-                    {translate(text)}
-                  </a>
-                </li>
-              );
-            })}
-          </ul>
-        </nav>
+        {!isDesktop && (
+          <button onClick={() => setMenuVisibility(!menuVisible)}>
+            {menuVisible ? 'Close' : 'Menu'}
+          </button>
+        )}
+        {menuVisible && (
+          <nav>
+            <ul className="flex">
+              {menuItems.map(({text, to}) => {
+                return (
+                  <li className="mx-6 text-white font-bold text-xl" key={text}>
+                    <a
+                      href={to}
+                      className="border-0 border-b-2 border-transparent hover:border-white"
+                    >
+                      {translate(text)}
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
+        )}
       </div>
     </div>
   );
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,23 @@ import '@testing-library/jest-dom/extend-expect';
 // easily mock the fetch API - https://github.com/jefflau/jest-fetch-mock
 import {enableFetchMocks} from 'jest-fetch-mock';
 enableFetchMocks();
+
+const findMatch = query => {
+  const value = query.match(/\d+/);
+  return /max/.test(query)
+    ? window.innerWidth <= value
+    : window.innerWidth >= value;
+};
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: findMatch(query),
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn()
+  }))
+});
