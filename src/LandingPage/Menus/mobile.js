406d15c28c688568b3a50b537c66753307843713
Kaspar Arme <kaspar.arme@reach-u.com>
2020-04-24 12:50:56 +0300

    Update mobile menu

--- src/LandingPage/Menus/mobile.js
+++ src/LandingPage/Menus/mobile.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Portal from '../../Portal';
 import {menuItems} from '../index';
 import {Icon} from '../../components/icons';
+import logo from '../../assets/images/Cofind_logo_roh_pos.png';
 
 export const MobileMenu = () => {
   const {translate} = useLocale();
@@ -24,6 +25,22 @@ export const MobileMenu = () => {
       {menuVisible ? (
         <Portal id="menu">
           <div className="bg-white fixed top-0 left-0 w-full h-full flex flex-col items-center justify-center">
+            <div className="absolute top-0 flex items-end w-full justify-between px-4 lg:px-16 py-6">
+              <img
+                className="h-20 lg:h-24"
+                src={logo}
+                alt={translate('logo')}
+              />
+              <button
+                className="text-lg"
+                onClick={() => setMenuVisibility(false)}
+              >
+                <div className="text-black">
+                  <Icon.Close className="mx-auto w-12" />
+                  <span>{translate('close')}</span>
+                </div>
+              </button>
+            </div>
             <nav className="flex items-center justify-center">
               <ul className="flex-col text-2xl">
                 {menuItems.map(({text, to}) => {
@@ -44,15 +61,6 @@ export const MobileMenu = () => {
                 })}
               </ul>
             </nav>
-            <button
-              className="text-lg"
-              onClick={() => setMenuVisibility(false)}
-            >
-              <div className="text-black">
-                <Icon.Close className="mx-auto w-12" />
-                <span>{translate('close')}</span>
-              </div>
-            </button>
           </div>
         </Portal>
       ) : (
