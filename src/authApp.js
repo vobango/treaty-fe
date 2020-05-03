1f874cdc0a07b7f692110a86c5cde7e7c7d0ea64
Krister Looga <looga.krister@gmail.com>
2020-05-04 00:00:30 +0300

    Changed to firestore, can add items to db

--- src/authApp.js
+++ src/authApp.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {withAuthorization} from './components/Session';
 import {withFirebase} from './components/Firebase';
 
-
 // this is a mock page to show case protected routes, where app should be
 const AppAuth = ({firebase}) => {
+  const [addPost, setAddPost] = useState('');
+
+  const sendPost = () => {
+    console.log(addPost);
+    if (addPost.length > 0) {
+      console.log('here');
+      firebase.doCreatePost(addPost);
+    }
+  };
   return (
     <div>
-      <h1>auth</h1>
+      <h1>Auth protected page</h1>
       <button type="button" onClick={firebase.doSignOut}>
         Sign Out
       </button>
+      <div>
+        <h1>Add post</h1>
+        <input value={addPost} onChange={e => setAddPost(e.target.value)} />
+        <button onClick={() => sendPost()}>Send post</button>
+      </div>
+      <div>
+        <h1>See all other user posts</h1>
+        <p>Note you can only delete posts created by you</p>
+      </div>
     </div>
   );
 };
--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,5 +1,6 @@
 import app from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyC6fQSbiesH2wVYNUWs0ul3oeDr0LpwJ0g',
@@ -11,20 +12,37 @@ const firebaseConfig = {
   appId: '1:550090633897:web:1708be98301a83aa012fb8'
 };
 
-
 class Firebase {
   constructor() {
     app.initializeApp(firebaseConfig);
     this.auth = app.auth();
+
+    this.db = app.firestore();
   }
   // *** Auth API ***
   doCreateUserWithEmailAndPassword = (email, password) =>
-      this.auth.createUserWithEmailAndPassword(email, password);
+    this.auth.createUserWithEmailAndPassword(email, password);
   doSignInWithEmailAndPassword = (email, password) =>
-      this.auth.signInWithEmailAndPassword(email, password);
+    this.auth.signInWithEmailAndPassword(email, password);
   doSignOut = () => this.auth.signOut();
   doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
-  doPasswordUpdate = password =>
-      this.auth.currentUser.updatePassword(password);
+  doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
+
+  // *** Post API ***
+  doCreatePost = post => {
+    console.log(this.auth.currentUser);
+    this.db
+      .collection('posts')
+      .add({
+        post: post,
+        email: this.auth.currentUser.email
+      })
+      .then(function(docRef) {
+        console.log('Document written with ID: ', docRef.id);
+      })
+      .catch(function(error) {
+        console.error('Error adding document: ', error);
+      });
+  };
 }
 export default Firebase;
