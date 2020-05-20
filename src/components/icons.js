5b54d4cd6047836c07ebf67d0091c698a52cd9cc
Kaspar Arme <kaspar.arme@reach-u.com>
2020-05-20 15:51:24 +0300

    Update listings list view

--- src/components/icons.js
+++ src/components/icons.js
@@ -131,6 +131,15 @@ const Loader = props => (
   </svg>
 );
 
+const Marker = props => (
+  <svg {...props} viewBox="0 0 24 24">
+    <path
+      fill="currentColor"
+      d="M12 11.5A2.5 2.5 0 019.5 9 2.5 2.5 0 0112 6.5 2.5 2.5 0 0114.5 9a2.5 2.5 0 01-2.5 2.5M12 2a7 7 0 00-7 7c0 5.25 7 13 7 13s7-7.75 7-13a7 7 0 00-7-7z"
+    />
+  </svg>
+);
+
 const Menu = props => (
   <svg {...props} viewBox="0 0 24 24">
     <path fill="currentColor" d="M3 6h18v2H3V6m0 5h18v2H3v-2m0 5h18v2H3v-2z" />
@@ -227,6 +236,7 @@ export const Icon = {
   Cross,
   ListPoint,
   Loader,
+  Marker,
   Menu,
   Number,
   Search,
--- src/pages/ViewWork.js
+++ src/pages/ViewWork.js
@@ -2,21 +2,27 @@ import React, {useEffect, useState} from 'react';
 import Layout from '../components/layout';
 import {withFirebase} from '../providers/firebase';
 import {withAuthorization} from '../components/session';
-import {condition} from '../components/session/withAuthorization';
+import {Icon} from '../components/icons';
+import {Link} from 'react-router-dom';
 
 const ViewWork = ({firebase}) => {
   const [posts, setPosts] = useState([]);
+  const format = date =>
+    new Intl.DateTimeFormat('en-GB').format(new Date(date));
 
   useEffect(() => {
     // Live updates
     const unsubscribe = firebase.db.collection('posts').onSnapshot(snapshot => {
+      let readData = [];
+
       if (snapshot.size) {
-        let readData = [];
         snapshot.forEach(doc => readData.push({...doc.data()}));
-        setPosts(readData);
-      } else {
-        setPosts([]);
       }
+
+      readData.sort((a, b) => {
+        return b.created - a.created;
+      });
+      setPosts(readData);
     });
     return () => {
       unsubscribe();
@@ -26,33 +32,65 @@ const ViewWork = ({firebase}) => {
   return (
     <Layout>
       <div>
-        <h1>See all other user posts</h1>
-        <h1>Currently you can only delete the last one</h1>
-        {posts.map(el => (
-          <div>
-            <p>I am a post created by user: {el.email}</p>
-            <p>{el.post}</p>
-            {el.created && (
-              <p>
-                The post was created at: {new Date(el.created).toDateString()}
-              </p>
-            )}
-            <button
-              className="entry-button"
-              onClick={() =>
-                firebase.doDeletePost(
-                  el.created,
-                  firebase.auth.currentUser.email
-                )
-              }
+        <h1 className="text-3xl">Kuulutused</h1>
+        {posts.map((post, i) => {
+          console.log(post);
+          const {
+            created,
+            workArea,
+            workerCount,
+            dateRange = [],
+            post: info
+          } = post;
+          const [from, to] = dateRange;
+          const itemClasses = 'flex items-center mr-3';
+          const iconClasses = 'h-6 w-6 text-gray-600';
+
+          return (
+            <div
+              key={created}
+              className="shadow-lg rounded-lg bg-green-100 p-6 my-8"
             >
-              DELETE POST
-            </button>
-          </div>
-        ))}
+              <h2 className="text-xl">{info || `Kuulutus ${i + 1}`}</h2>
+              <div className="text-gray-600 text-sm mb-6">
+                Lisatud {format(created)}
+              </div>
+              <div className="flex">
+                {!!workArea && (
+                  <div className={itemClasses}>
+                    <Icon.Marker className={iconClasses} />
+                    {workArea}
+                  </div>
+                )}
+                {!!workerCount && (
+                  <div className={itemClasses}>
+                    <Icon.Worker className={iconClasses} />
+                    {workerCount}
+                  </div>
+                )}
+                {!!from && !!to && (
+                  <div className={itemClasses}>
+                    <Icon.Calendar className={iconClasses} />
+                    {format(from)} - {format(to)}
+                  </div>
+                )}
+              </div>
+              <Link
+                to={`posts/${123}`}
+                className="flex justify-center items-center py-1 px-3 mt-6 rounded-full border-2 border-green-500 text-gray-800 text-sm w-1/2"
+              >
+                Vaata lähemalt{' '}
+                <Icon.Arrow
+                  direction="right"
+                  className="w-5 h-5 ml-2 text-gray-600"
+                />
+              </Link>
+            </div>
+          );
+        })}
       </div>
     </Layout>
   );
 };
 
-export default withFirebase(withAuthorization(condition)(ViewWork));
+export default withFirebase(withAuthorization()(ViewWork));
